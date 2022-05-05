795c099818ffd8c18a491e1cdc48109cc02b3fbd
Ahmed Elsayed <ahmedelsayed3157@gmail.com>
2022-05-05 12:59:39 +0200

    make all api config

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,68 @@
+import NextAuth from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+import spotifyAPI, { LOGIN_URL } from '../../../lib/spotify'
+
+async function refreshAccessToken(token) {
+  try {
+    spotifyAPI.setAccessToken(token.accessToken)
+    spotifyAPI.setRefreshToken(token.refreshToken)
+    const { body: refreshedToken } = await spotifyAPI.refreshAccessToken()
+
+    return {
+      ...token,
+      accessToken: refreshedToken.access_token,
+      accesstokenExpires: Date.now() + refreshedToken.expires_in * 1000,
+      refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+    }
+  } catch (error) {
+    console.error(error)
+    return {
+      ...token,
+      error: 'refresh Access Token Error ',
+    }
+  }
+}
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.next_public_ID,
+      clientSecret: process.env.Next_public_secret,
+      authorization: LOGIN_URL,
+    }),
+    // ...add more providers here
+  ],
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    async jwt({ token, account, user }) {
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accountTokenExpires: account.expires_at * 1000,
+        }
+      }
+
+      if (Date.now() < token.accountTokenExpires) {
+        console.log('Existing access token is valid ')
+        return token
+      }
+
+      console.log('Access token has expired , Refreshing ...')
+      return await refreshAccessToken(token)
+    },
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken
+      session.user.refreshToken = token.refreshToken
+      session.user.username = token.username
+
+      return session
+    },
+  },
+})
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,21 @@
-const login = () => {
+import { getProviders, signIn } from 'next-auth/react'
+
+const login = ({ providers }: any) => {
   return (
     <div>
-      <h1>This is login page </h1>
+      <img src="https:/links.papareact.com/9xl" alt="logo" className="" />
     </div>
   )
 }
 
 export default login
+
+export async function getServerSideProps() {
+  const providers = await getProviders()
+
+  return {
+    props: {
+      providers,
+    },
+  }
+}
